e7183449038c0dc91f034f1ad5297b2bfda3673b
Alejandro Torras <1059709+atorr0@users.noreply.github.com>
2019-12-16 01:09:07 +0100

    FIX loading issues with DllMain by using fci_init

--- fci_cygwin.c
+++ fci_cygwin.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdarg.h>
-#include <sys/mount.h>
+// #include <sys/mount.h>
 // #include <unistd.h>
 
 #define LOGCMD(__log_cmd)              \
@@ -22,7 +22,7 @@ int fci_open(const char *path, int flags, ...)
     va_list ap;
     va_start(ap, flags);
 
-    LOGCMD(printf("Opening %s", path));
+    LOGCMD(printf("Opening %s with flags %d", path, flags));
 
     int _open = open(path, flags, ap);
     va_end(ap);
@@ -32,44 +32,18 @@ int fci_open(const char *path, int flags, ...)
 
 FILE *fci_fopen(const char *path, const char *mode)
 {
-    LOGCMD(printf("Opening %s", path));
+    LOGCMD(printf("Opening %s with mode %s", path, mode));
 
     return fopen(path, mode);
 }
 
-int fci_mount (const char *name, const char *opts, unsigned __flags){
-
-    LOGCMD(printf("Mounting %s [%s] %u", name, opts, __flags));
-
-    // if name starts with "fci:" then hook it, else do default
-    // fictional example:  fci:dummyfs:-readonly,-mem=1024
-    if (strncmp(name, "fci:", 4) == 0) {
-        // TODO update current bash LD_PRELOAD
-        return -1; 
-    }
-
-    return mount(name, opts, __flags);
-}
-
-BOOLEAN WINAPI DllMain(IN HINSTANCE hDllHandle,
-                       IN DWORD nReason,
-                       IN LPVOID Reserved)
+static void
+__attribute__((constructor)) 
+fci_init(void)
 {
-    if (nReason == DLL_PROCESS_ATTACH)
-    {
-        LOG("compilation (" __DATE__ " " __TIME__ ")");
-        LOG("DLL_PROCESS_ATTACH start");
+    LOG("compilation (" __DATE__ " " __TIME__ ")");
+    LOG("fci_init");
 
-        cygwin_internal(CW_HOOK, "fopen", fci_fopen);
-        cygwin_internal(CW_HOOK, "open", fci_open);
-        cygwin_internal(CW_HOOK, "mount", fci_mount);
-    }
-    else if (nReason == DLL_PROCESS_DETACH)
-    {
-        FreeLibrary(hDllHandle);
-    }
-
-    LOGCMD(printf("End (reason=%u)", nReason));
-
-    return TRUE;
+    cygwin_internal(CW_HOOK, "fopen", fci_fopen);
+    cygwin_internal(CW_HOOK, "open", fci_open);
 }
